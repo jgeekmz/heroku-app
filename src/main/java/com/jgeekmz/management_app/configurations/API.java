eabab536cc670795c84adff0313d60ad11a722f0
Martin <martin.zlatkov@gmail.com>
2021-06-04 17:16:07 +0200

    MailGunConfig Settings & Testing

--- src/main/java/com/jgeekmz/management_app/configurations/API.java
+++ src/main/java/com/jgeekmz/management_app/configurations/API.java
@@ -1,5 +1,7 @@
 package com.jgeekmz.management_app.configurations;
 
 public class API {
-    final public static String API_KEY = "a72d7eb32df41e43434914d2b8b9169e-1d8af1f4-9492ece8";
+
+    final public static String API_KEY = "4f0bb0152346c46721202341b5c469b7-1d8af1f4-512c10e5";
+
 }
